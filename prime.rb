1ee0f08b83358995d8e7cdb1708b4bb2369f9520
Jonathan East <jonathan_east@ymail.com>
2020-05-11 01:23:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,12 @@
 # Add  code here!
 def prime?(num)
   return false if num <= 1
+
+  i = 2 #set constant at 2 since anything below two is returned false
+  while i < num
+    if i % num == 0
+        return false
+    end
+  end
+  return true
 end
